609a33ec57be852b06a0b169ad9fd852244aee2d
nirnanaaa <mosny@zyg.li>
2014-01-16 14:42:07 +0100

    added meetings

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,4 +1,79 @@
 class MeetingsController < ApplicationController
   unloadable
 
+  before_filter :set_gollum_path
+  before_filter :find_project
+  def index
+    Page.wiki.clear_cache
+    @pages = Page.all
+  end
+  def show
+    
+  end
+  def create
+    if params[:description][:description].empty? 
+      flash[:error] = l(:error_description_cannot_be_empty)
+      session[:meeting_description_content] = params[:description][:content]
+      redirect_to new_meeting_protocol_path(@project)
+    else
+      @page = Page.create!(name: date(params[:description][:description]), format: :markdown, content: params[:description][:content], commit: commit_for(:create))
+      flash[:notice] = l(:notice_meeting_successfully_saved)
+      redirect_to meetings_path(@project)
+    end
+  rescue Gollum::DuplicatePageError => e
+    flash[:error] = l(:error_page_already_exists)
+    redirect_to meetings_path(@project)
+  end
+  def new
+    
+  end
+  def update
+  end
+  def destroy
+    page = Page.find(params[:meeting_id])
+    render text: page.to_yaml
+    #page.delete(commit_for(:destroy))
+    #puppetredirect_to meetings_path(@project)
+  end
+  
+  def edit
+  end
+  
+  private
+  
+  def set_gollum_path
+    GollumRails::Setup.wiki_options = { :sanitization => false, :base_path => '', :page_file_dir => "runden/#{params[:project_id]}" }
+  end
+  
+  def date(description)
+    t = Time.now
+    "#{t.day}_#{t.month}_#{t.year}__#{description.gsub(' ', '_')}"
+  end
+  
+
+  
+  def find_project
+    project_id = params[:project_id] || (params[:meeting] && params[:meeting][:project_id])
+    @project = Project.find(project_id)
+  rescue ActiveRecord::RecordNotFound
+    render_404
+  end
+  
+  def commit_for(action)
+    current_user = User.current
+    case action
+      when :create
+        a = "Created meeting protocol"
+      when :update
+        a = "Updated meeting protocol"
+      when :destroy
+        a = "Destroyed meeting protocol"
+    end
+    {
+      name: "#{current_user.firstname} #{current_user.lastname}",
+      email: current_user.mail,
+      message: a
+    }
+  end
+  
 end
--- app/helpers/meetings_helper.rb
+++ app/helpers/meetings_helper.rb
@@ -1,2 +1,14 @@
 module MeetingsHelper
+  def seperate_date(name)
+    dname(name).first.gsub(/\_/, ".")
+  end
+  def dname(name)
+    name.split("__")
+  end
+  
+  def svar_clean(session_variable)
+    sv = session[session_variable]
+    session[session_variable] = nil
+    sv
+  end
 end
