2cc0c46fc420a8e26a01766d29d9646241c83238
nirnanaaa <mosny@zyg.li>
2014-01-20 09:59:35 +0100

    added meeting model

---
+++ app/models/meeting.rb
@@ -0,0 +1,3 @@
+class Meeting < ActiveRecord::Base
+  unloadable
+end
---
+++ db/migrate/001_create_meetings.rb
@@ -0,0 +1,6 @@
+class CreateMeetings < ActiveRecord::Migration
+  def change
+    create_table :meetings do |t|
+    end
+  end
+end
