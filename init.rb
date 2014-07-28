cd29734d27d226c054863aa72dadff1829aa0d9a
nirnanaaa <mosny@zyg.li>
2014-07-28 16:25:52 +0200

    minified output

--- init.rb
+++ init.rb
@@ -12,25 +12,19 @@ Redmine::Plugin.register :gollum do
   version '0.9.0'
   url 'http://example.com/path/to/plugin'
   author_url 'http://floriankasper.org'
-  
   menu :top_menu, :wiki, { :controller => 'pages', :action => 'index' }, :caption => :label_menu_gollum
   permission :polls, { :polls => [:index, :vote] }, :public => true
   menu :project_menu, :polls, { :controller => 'pages', :action => 'index' }, :caption => 'Polls', :after => :activity, :param => :project_id
 
   project_module :meetings do
     permission :view_meetings, {:meetings => [:index, :show] }
-      
     permission :edit_meeting,
       {:meetings => [:create, :destroy, :new, :toggle_complete, :sort, :edit, :update],
         :issues => [:create, :destroy, :new, :toggle_complete, :sort, :edit, :update]}
-   
   end
 
-     
   menu :project_menu, :meetings, {:controller => 'meetings', :action => 'index'}, 
       :caption => :label_project_menu_meetings, :after => :activity, :param => :project_id
-      
-    
 
   settings :default => {
     'wiki_root' => Rails.root.join('wiki.git'),
