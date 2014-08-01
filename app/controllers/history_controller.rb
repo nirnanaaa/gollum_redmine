b6b49f635fe68cadd583acca4179e5ba56a716d4
nirnanaaa <mosny@zyg.li>
2014-08-01 07:01:49 +0200

    udpated folder icons

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -11,18 +11,19 @@ class HistoryController < ApplicationController
   
   def global
     wiki = Gpage.wiki
-    options = { pretty: 'raw' }
-    log = wiki.repo.git.log(options, wiki.ref, "--")
-    @history = Grit::Commit.list_from_string(wiki.repo, log)
-    
+
+    repo = wiki.repo
+    walker = Rugged::Walker.new(repo)
+    walker.sorting(Rugged::SORT_DATE)
+    walker.push(repo.last_commit)
+    @history = walker.inject([]) do |a, c|
+      a << {author: c.author, hash: c.oid, subject: c.message.split("\n").first}
+      a
+    end
     render :show
   end
-  
-  
   private
-  
   def reset_path
     GollumRails::Setup.wiki_options = { :page_file_dir => nil }
   end
-  
 end
